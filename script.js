e6162ad92e4f2d28b27a3a2caae9e15f04aa5b1d
vimal772 <vimal115077286@gmail.com>
2023-10-06 17:35:56 +0530

    Grid style is formed Basic is completed

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("connected");
+const container = document.querySelector('#container');
+
+let size = 16;
+
+for(let i=0;i<size;i++){
+    const colDiv = document.createElement('div');
+    colDiv.id = "col"+(i+1);
+    for(let j=0;j<size;j++){
+        const newDiv = document.createElement('div');
+        newDiv.id = 'row'+(i+1);
+        newDiv.textContent = "DIV"+i;
+
+        colDiv.appendChild(newDiv)
+    }
+    container.appendChild(colDiv);
+}
