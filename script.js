7ab11141aeeded8edd6c395194fff593d174849c
vimal772 <vimal115077286@gmail.com>
2023-10-09 20:32:24 +0530

    user size grid is added can change any time

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 console.log("connected");
 const container = document.querySelector('.container');
-let sizeOfGrid = 2;
+const reset = document.querySelector('.reset');
 
 function createGrid(gridSize){
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
     for(let i=0;i<gridSize;i++){
         const rowGrid = document.createElement('div');
         rowGrid.classList.add('grid-row');
         for(let j=0;j<gridSize;j++){
-            const width = 960/sizeOfGrid;
+            const width = 960/gridSize;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
             gridBox.style.width = `${width}px`;
             gridBox.style.height = `${width}px`;
+            //event added for color change while hover
+            gridBox.addEventListener('mouseenter',()=>{
+                gridBox.style.backgroundColor = 'black';
+            });
             rowGrid.appendChild(gridBox);
         }
-        container.appendChild(rowGrid);
+        wrapper.appendChild(rowGrid);
     }
+    container.appendChild(wrapper);
 };
 
-createGrid(sizeOfGrid);
+
+reset.addEventListener('click',()=>{
+    let input =document.querySelector('.input');
+    let userSize = Number(input.value);
+
+    while(userSize>100){
+        alert("Give the value less than 100");
+        userSize=16;
+        input.value='';
+    }
+    const wrapper = document.querySelector('.wrapper');
+
+    if(!wrapper){
+        createGrid(userSize);
+    }else{
+        wrapper.remove();
+        createGrid(userSize);
+    }
+})
